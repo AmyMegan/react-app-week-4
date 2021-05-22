45be53b4f8a8202c83aaa1316aa14d8dc75f3599
AmyMegan <amycostall@outlook.com>
2021-05-22 17:23:04 +0100

    Updated React

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
         <div className="upper">
           <h1 div="current">
             <br />
-            <img src="https://ssl.gstatic.com/onebox/weather/48/sunny.png"  alt="image of the weather elements"/>
+            <img src="https://ssl.gstatic.com/onebox/weather/48/sunny.png"  alt="weather elements"/>
             {weatherData.description}
             <span>
               {" "}
@@ -56,7 +56,7 @@ function App() {
           <li className="info">
             <img
               src="https://img.icons8.com/color/50/000000/hygrometer.png"
-              alt="humidity"
+              alt="weather elements"
               width="50px"
             />
             <div>{weatherData.humidity}</div>
@@ -64,7 +64,7 @@ function App() {
           <li className="info2">
             <img
               src="https://cdn4.iconfinder.com/data/icons/the-weather-is-nice-today/64/weather_41-512.png"
-              alt="wind"
+              alt="weather elements"
               width="40px"
             />
             <div>{weatherData.wind}</div>
@@ -74,7 +74,7 @@ function App() {
           <div className="row">
             <div className="col">
               <div>
-                <img src={weatherData.imgURL} alt="image of weather elements"/>
+                <img src={weatherData.imgURL} alt="weather elements"/>
                 <p className="temp"> Sunny </p>
                 <footer>
                   I'm on
